255e9770fc3b968e309dc2c3d9da9c9750a5cd35
RobertEduardSimion <zx22student3218@colegiovalledelmiro.es>
2023-10-03 09:44:48 +0200

    todo lo demas y la division

--- Calculadora.py
+++ Calculadora.py
@@ -9,6 +9,8 @@ print(n2)
 print("¿Qué quieres hacer")
 print("1---Sumar")
 print("2---Restar")
+print("3---Multiplicar")
+print("4---Dividir")
 
 operacion = input()
 
@@ -25,3 +27,7 @@ if operacion == "Restar" or operacion == "restar" or operacion == "2" :
 #MULTIPLICACIÓN
 if operacion == "Multiplicar" or operacion == "multiplicar" or operacion == "3" :
     print(salida.format(int(n1*n2)))
+
+#DIVISIÓN
+if operacion == "Dividir" or operacion == "dividir" or operacion == "4" :
+    print(salida.format(int(n1/n2)))
