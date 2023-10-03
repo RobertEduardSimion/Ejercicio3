25428b3da04cda60f1f4354b9b00fff30ae81749
RobertEduardSimion <zx22student3218@colegiovalledelmiro.es>
2023-10-03 08:42:22 +0200

    suma calculadora

---
+++ Calculadora.py
@@ -0,0 +1,13 @@
+#primer numero
+n1 = int(input("Dame un numero: "))
+print(n1)
+
+#segundo numero
+n2 = int(input("Dame otro numero: "))
+print(n2)
+
+#operacion
+suma = n1 + n2
+
+#imprimir resultado
+print("El resultado es "+ str(suma))
